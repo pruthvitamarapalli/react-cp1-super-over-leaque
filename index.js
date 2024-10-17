2d585931fe8984a9dcde1a665d5d8ec71cce76c8
pruthvi <pruthvitamarapalli@gmail.com>
2024-10-17 06:02:58 +0000

    solution

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+  //   Write your code here.
+  <div>
+    <h1> Super Over Leaque </h1>
+    <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' />
+    <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' />
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
